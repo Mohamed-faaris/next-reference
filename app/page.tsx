b913de5700873706da729af06cf9a7eed6130ea8
mohamed-faaris <mohammedfaaris2005@gmail.com>
2025-05-18 01:49:25 +0530

    refactor: clean up code formatting and improve API fetch handling in Home component

--- app/page.tsx
+++ app/page.tsx
@@ -6,11 +6,11 @@ import { useState } from "react";
 export default function Home() {
   const router = useRouter();
   const [url, setUrl] = useState("");
-  const [data, setData] = useState("")
+  const [data, setData] = useState("");
   const [param, setParam] = useState("");
   const [body, setBody] = useState("");
   function handleClick() {
-    if(url){
+    if (url) {
       router.push(`dynamicUrl/${url}`);
     }
   }
@@ -34,7 +34,9 @@ export default function Home() {
 
         <button
           className="bg-white text-black p-2 rounded-xl m-2"
-          onClick={()=> {router.push(`dynamicUrlSSR/${url}`)}}
+          onClick={() => {
+            router.push(`dynamicUrlSSR/${url}`);
+          }}
         >
           push to SSR
         </button>
@@ -47,8 +49,7 @@ export default function Home() {
           value={url}
           onChange={(e) => {
             setUrl(e.target.value);
-          }
-          }
+          }}
         />
         <input
           type="text"
@@ -56,54 +57,52 @@ export default function Home() {
           value={param}
           onChange={(e) => {
             setParam(e.target.value);
-          }
-          }
+          }}
         />
         <button
-          className="bg-white text-black p-2 rounded-xl m-2"  
-          onClick={async() => {
-            const res = await fetch(`http://localhost:3000/api/dynamicUrl/${url}?param=${param}`);
+          className="bg-white text-black p-2 rounded-xl m-2"
+          onClick={async () => {
+            const res = await fetch(`/api/dynamicUrl/${url}?param=${param}`);
             const data = await res.json();
             setData(data.message);
           }}
         >
           get API
         </button>
-      <div>
-        <input 
-          type="text"
-          placeholder="body"
-          value={body}
-          onChange={(e) => {
-            setBody(e.target.value);
-          }
-          }
-        />
-        <button
-          className="bg-white text-black p-2 rounded-xl m-2"  
-          onClick={async() => {
-            const res = await fetch(`http://localhost:3000/api/dynamicUrl/${url}`, {
-              method: "POST",
-              body: JSON.stringify({ body }),
-              headers: {
-                "Content-Type": "application/json",
-              },
-            });
-            const data:any = await res.json();
-            setData(data);
-          }}
-        >
-          post API
-        </button>
-
+        <div>
+          <input
+            type="text"
+            placeholder="body"
+            value={body}
+            onChange={(e) => {
+              setBody(e.target.value);
+            }}
+          />
+          <button
+            className="bg-white text-black p-2 rounded-xl m-2"
+            onClick={async () => {
+              const res = await fetch(
+                `http://localhost:3000/api/dynamicUrl/${url}`,
+                {
+                  method: "POST",
+                  body: JSON.stringify({ body }),
+                  headers: {
+                    "Content-Type": "application/json",
+                  },
+                }
+              );
+              const data: any = await res.json();
+              setData(data);
+            }}
+          >
+            post API
+          </button>
+        </div>
+        <div>
+          {data && <div className="text-2xl">Data from API:</div>}
+          {data && JSON.stringify(data)}
+        </div>
       </div>
-      <div>
-        {data && <div className="text-2xl">Data from API:</div>}
-        {data && JSON.stringify(data)}
-      </div>
-        
-    </div>
     </div>
   );
 }
-
